95e0158fc2bc8b296fefec4b1055a63239b74842
Yuichiro Saito <saito@heartbeats.jp>
2015-09-23 22:48:34 +0900

    p67のコードを読みつつ修正

--- p67/main.scala
+++ p67/main.scala
@@ -2,7 +2,8 @@ import learningconcurrency._
 
 object ExecutionContextCreate extends App {
   import scala.concurrent._
-  val ectx = ExecutionContext.fromExecutorService(new forkjoin.ForkJoinPool)
+  val pool = new forkjoin.ForkJoinPool(2)
+  val ectx = ExecutionContext.fromExecutorService(pool)
   ectx.execute(new Runnable {
     def run() = log("Running on the execution context again.")
   })
