2ab0c0772196369ed8fd95a5cfb3efec3cfeadd6
Yuichiro Saito <saito@heartbeats.jp>
2015-09-23 22:51:13 +0900

    sleep関連読み込んだ結果を反映

--- p65_2/main.scala
+++ p65_2/main.scala
@@ -2,9 +2,11 @@ import learningconcurrency._
 
 object ExecutorsCreate extends App {
   import scala.concurrent._
+  import java.util.concurrent.TimeUnit
   val executor = new java.util.concurrent.ForkJoinPool
   executor.execute(new Runnable {
     def run() = log("This task is run asynchronously.")
   })
-  // Thread.sleep(500)
+  executor.shutdown()
+  executor.awaitTermination(60, TimeUnit.SECONDS)
 }
